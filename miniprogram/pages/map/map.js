b88b182c5655a9625a8c051d13642f2a12977bc1
GQMai <gq.mai@aliyun.com>
2024-06-02 22:30:18 +0800

    Implemented basic features

--- miniprogram/pages/map/map.js
+++ miniprogram/pages/map/map.js
@@ -1,43 +1,57 @@
+const constants = require('./constants.js');
+
 const app = getApp() // 获取全局APP对象
 let that = null // 页面this指针变量
 Page({
   data: { // 默认数据
-    latitude: 39.9086, // 地图中心纬度
-    longitude: 116.3974, // 地图中心经度
+    latitude: 22.5529, // 地图中心纬度
+    longitude: 114.0893, // 地图中心经度
     location: '', // 经纬度输入框
     address: '', // 地址输入框
     model: 0, // 模式转换 0-地址输入，1-经纬度输入
-    marker: { // 地图当前标记点
-      id: 0, // 标记点ID，不用变更
-      latitude: 39.9086, // 标记点所在纬度
-      longitude: 116.3974, // 标记点所在经度
-      iconPath: '../../asset/local.png', // 标记点图标，png或jpg类型
-      width: '20', // 标记点图标宽度
-      height: '20' // 标记点图标高度
-    },
-    info: { // 地图点位信息
-      address: '-', // 常规地址
-      adinfo: '-', // 行政区
-      formatted: '-', // 推荐地址
-      location: '-' // 经纬度
+    markers: [],
+    info: {
+      name: '-',
+      class: '-',
+      address: '-',
+      street: '-',
+      type: '-',
+      fee: '-',
+      phone: '-',
+      organizer: '-',
     }
+  },
+  markerTap: function (e) {
+    let school = constants.SCHOOLS[e.markerId]
+    this.setData({
+      info: {
+        name: school.name,
+        class: school.class,
+        address: school.address,
+        phone: school.phone,
+        type: school.type,
+        fee: school.fee,
+        organizer: school.organizer,
+        street: school.street,
+      }
+    });
   },
   /**
    * 页面装载回调
    */
-  onLoad () {
+  onLoad() {
     that = this // 设置页面this指针到全局that
     wx.getLocation({ // 获取当前位置
       type: 'gcj02', // gcj02火星坐标系，用于地图标记点位
-      success (res) { // 获取成功
+      success(res) { // 获取成功
         that.setInfo([parseFloat(res.latitude), parseFloat(res.longitude)]) // 设置经纬度信息
         that.getLocation() // 获取当前位置点
       },
-      fail (e) { // 获取失败
+      fail(e) { // 获取失败
         if (e.errMsg.indexOf('auth deny') !== -1) { // 如果是权限拒绝
           wx.showModal({ // 显示提示
             content: '你已经拒绝了定位权限，将无法获取到你的位置信息，可以选择前往开启',
-            success (res) {
+            success(res) {
               if (res.confirm) { // 确认后
                 wx.openSetting() // 打开设置页，方便用户开启定位
               }
@@ -46,11 +60,40 @@ Page({
         }
       }
     })
+
+    var markers = constants.SCHOOLS.map(function (item, index) {
+      if(item.latitude  == null || item.latitude  == null){
+        return null
+      }
+
+      return {
+        id: index,
+        latitude: item.latitude,
+        longitude: item.longitude,
+        title: item.name,
+        iconPath: getIconPathBySchoolType(item.type),
+        //<a target="_blank" href="https://icons8.com/icon/iAjopNFQNw6O/school-location">School Location</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+        width: '40',
+        height: '40',
+        callout: {
+          display: 'ALWAYS',
+          content: item.name,
+          padding: 5,
+          borderRadius: 5
+        },
+      };
+    }).filter(function(element) {
+      return element !== null;
+    });
+
+    this.setData({
+      markers: markers
+    });
   },
   /**
    * 改变输入框类型
    */
-  changemodel () {
+  changemodel() {
     that.setData({
       model: that.data.model === 0 ? 1 : 0 // 如果为0则设置1，如果为1则设置0
     })
@@ -59,29 +102,37 @@ Page({
    * 点击地图
    * @param {*} e 页面载入参数
    */
-  clickMap (e) {
-    const { latitude, longitude } = e.detail // 获取点击的经纬度信息
+  clickMap(e) {
+    const {
+      latitude,
+      longitude
+    } = e.detail // 获取点击的经纬度信息
     let data = {}
     if (that.data.model === 1) { // 如果模式在经纬度输入
-      data = { location: `${latitude.toFixed(4)},${longitude.toFixed(4)}` } // 写当前经纬度信息
+      data = {
+        location: `${latitude.toFixed(4)},${longitude.toFixed(4)}`
+      } // 写当前经纬度信息
     }
-    that.setInfo([latitude, longitude], 2, data) // 更改标记点信息
-    that.getLocation({ latitude, longitude }) // 调用方法，传入经纬度，更新地址信息
+    // that.setInfo([latitude, longitude], 2, data) // 更改标记点信息
+    // that.getLocation({ latitude, longitude }) // 调用方法，传入经纬度，更新地址信息
   },
   /**
    * 地图范围改变
    * @param {*} e 页面载入参数
    */
-  changeMap (e) {
+  changeMap(e) {
     if (e.causedBy === 'drag' && e.type === 'end') { // 只有在手动拖动停止时，才执行动作
-      const { latitude, longitude } = e.detail.centerLocation // 获取中心点位置
+      const {
+        latitude,
+        longitude
+      } = e.detail.centerLocation // 获取中心点位置
       that.setInfo([latitude, longitude], 1) // 不更改标记点，更新地图中心位置
     }
   },
   /**
    * 手动点击查询按钮，查询信息
    */
-  async query () {
+  async query() {
     if (that.data.model === 0) { // 如果模式为地址查询
       that.getAddress(that.data.address) // 调用地址请求方法，传入地址信息
     } else if (that.data.model === 1) { // 如果模式为经纬度查询
@@ -117,7 +168,7 @@ Page({
    * 请求获取经纬度的详细信息
    * @param {object} data 经纬度信息
    */
-  async getLocation (data = null) {
+  async getLocation(data = null) {
     const {
       latitude,
       longitude
@@ -138,7 +189,7 @@ Page({
    * 请求获取地址的详细信息
    * @param {*} address 地址信息
    */
-  async getAddress (address) {
+  async getAddress(address) {
     await app.call({ // 发起云函数请求
       name: 'address', // 业务为address，获取地址信息
       data: { // 传入地址信息
@@ -155,7 +206,7 @@ Page({
    * 监听输入框
    * @param {*} e 页面载入参数
    */
-  oninput (e) {
+  oninput(e) {
     that.setData({
       [e.currentTarget.dataset.key]: e.detail.value
     })
@@ -166,8 +217,10 @@ Page({
    * @param {number} type 类型 0-都设置 1-只设置中心点 2-只设置标记点
    * @param {*} ext 额外的其他数据，一块带入
    */
-  setInfo (pot = [39.9086, 116.3974], type = 0, ext = {}) {
-    let data = { ...ext }
+  setInfo(pot = [39.9086, 116.3974], type = 0, ext = {}) {
+    let data = {
+      ...ext
+    }
     if (type !== 1) { // 如果类型不为1
       data = Object.assign(data, { // 传入标记点
         'marker.latitude': pot[0],
@@ -183,3 +236,24 @@ Page({
     that.setData(data)
   }
 })
+
+function getIconPathBySchoolType(schoolTYpe) {
+  let iconPath = ""
+  switch (schoolTYpe) {
+    case '市属公办园':
+      iconPath = "../../asset/inclusive-school.png";
+      break;
+    case '区属公办园':
+      iconPath = "../../asset/public-school.png";
+      break;
+    case '民办普惠园':
+      iconPath = "../../asset/inclusive-school.png";
+      break;
+    case '民办非普惠园':
+      iconPath = "../../asset/private-school.png";
+      break;
+    default:
+      iconPath = "../../asset/rebuildng-school.png";
+  }
+  return iconPath;
+}
